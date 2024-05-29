e584357d12f9c6c1c4de5a79181538fd1feeed71
Viktor Prusak <viktor.prusak@gmail.com>
2024-05-29 18:28:29 +0200

    feat(): created new project

---
+++ Applifting/src/test/java/com/vp/applifting/AppliftingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vp.applifting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliftingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
